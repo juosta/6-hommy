2a948d82d0f3604eb8f4489284508dc7e992330a
juosta <79445286+juosta@users.noreply.github.com>
2021-03-06 17:34:48 +0200

    lists overlay

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function showtenants() {
+    document.getElementById('tenantslist').style.visibility='visible';
+    document.getElementById('organizationslist').style.visibility='hidden';
+  }
+  function showorganizations() {
+    document.getElementById('tenantslist').style.visibility='hidden';
+    document.getElementById('organizationslist').style.visibility='visible';
+  }
